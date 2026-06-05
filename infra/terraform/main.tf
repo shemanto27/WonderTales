a5c6e41580f61d8b0ef154682388f70746c9dc09
shemanto27 <shemantosharkarofficial@gmail.com>
2026-06-06 02:24:11 +0600

    chore: increase server resources and request timeouts to stabilize long-running backend processes

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -12,6 +12,11 @@ resource "aws_instance" "ec2_server_1" {
   key_name               = aws_key_pair.terraform_key.key_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
+  root_block_device {
+    volume_size = 25
+    volume_type = "gp3"
+  }
+
   tags = {
     Name = "wondertales"
   }
